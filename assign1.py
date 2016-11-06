d75464ee706259453f8947bb5f5b05bfdf5c9410
Aimable Rwema <arwema@gmail.com>
2016-11-06 18:54:36 +0000

    Added AC

--- assign1.py
+++ assign1.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 	file_name = "report_%s.csv" % datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
 	file = open(file_name, "a")
 	if os.stat(file_name).st_size == 0:        
-		file.write("Day,Hour,Dark,Cool,OccupantIn,LightOn,CorridorLightOn \n")
+		file.write("Day,Hour,Room,Dark,Cool,OccupantIn,LightOn,CorridorLightOn, AC \n")
 	day = 1
 
 	# Light the LED on and off in a random manner
@@ -79,18 +79,34 @@ if __name__ == "__main__":
 			for room in rooms:
 			    is_occupant_in = occupant_in(hour)
                             light_on = is_dark(hour) and is_occupant_in
+                            ac_on = not is_cool(hour) and is_occupant_in
 			    atleast_one_occupant = atleast_one_occupant or is_occupant_in 
+
+			    #office lights
 			    if light_on:
 			    	GPIO.output(room['light'],GPIO.HIGH)
 			    else:
 			    	GPIO.output(room['light'],GPIO.LOW)
+
+		            #corridor light
 			    if atleast_one_occupant and is_dark(hour):
 			    	GPIO.output(6,GPIO.HIGH)
 			    else:
 			    	GPIO.output(6,GPIO.LOW)
-			    file.write("%i,%i,%s,%s,%s,%s,%s \n" % (day,hour,str(is_dark(hour)), str(is_cool(hour)), str(occupant_in(hour)),str(light_on), str(atleast_one_occupant)))
+
+
+			    #office AC 
+			    if ac_on:
+			    	GPIO.output(room['ac'],GPIO.HIGH)
+			    else:
+			    	GPIO.output(room['ac'],GPIO.LOW)
+
+			    #write to csv file
+			    file.write("%i,%i,%s,%s,%s,%s,%s,%s,%s \n" % (day,hour,room['room_number'],str(is_dark(hour)), str(is_cool(hour)), str(occupant_in(hour)),str(light_on), str(atleast_one_occupant),str(ac_on)))
 			    file.flush() 
-			    print "Room Number %s Occupant is %s Ligh is %s" % (room['room_number'], "In" if is_occupant_in else "Out", "On" if light_on else "Off")
+
+			    #display results
+			    print "Room Number %s Occupant is %s Light: %s and AC: %s" % (room['room_number'], "In" if is_occupant_in else "Out", "On" if light_on else "Off", "ON" if ac_on else "OFF")
 	    		print "Time: %i:00 It is %s and %s" % (hour, "dark" if is_dark(hour) else "bright", "cool" if is_cool(hour) else "hot") 
 			time.sleep(1)
 
