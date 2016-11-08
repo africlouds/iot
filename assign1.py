63c450576e80ffe88b246cf4fb43094625724d1a
Aimable Rwema <arwema@gmail.com>
2016-11-08 14:31:44 +0000

    Added probability logics

--- assign1.py
+++ assign1.py
@@ -29,29 +29,43 @@ rooms = [
 
 
 shared_space = ['corridor', 'washrooms']
-PROBABILITY_WORK_AT_NIGHT = 0.1
-PROBABILITY_LIGHT_DAY_TIME = 0.1
-PROBABILITY_FORGET_SWITCH_OFF_LIGHT = 0.05
+
+#random probabilities in %
+P_WORK_DAYTIME = 80
+P_WORK_IN_EVENING = 50
+P_WORK_AT_NIGHT = 10
+P_FORGET_SWITCH_OFF_OFFICE_LIGHT = 5
+P_FORGET_SWITCH_OFF_CORRIDOR_LIGHT = 50
+
 room_states = []
 
-
 def is_dark(hour):
-	return True if 0 <= hour <=6 or 18 <= hour <= 23 else False
+	return 1 if 0 <= hour <=6 or 18 <= hour <= 23 else 0
 
 def is_cool(hour):
-	return False if 12 <= hour <= 17 else True 
+	return 0 if 12 <= hour <= 17 else 1 
 
 def occupant_in(hour):
 	choices = []
 	if 18 <= hour <= 21:
-		choices = [True] * 50 + [False] * 50
+		choices = [1] * P_WORK_IN_EVENING + [0] * (100  - P_WORK_IN_EVENING) 
 	elif hour >= 22 or hour <= 6:
-		choices = [False] * 90 + [True] * 10
+		choices = [0] * (100 - P_WORK_AT_NIGHT) + [1] * P_WORK_AT_NIGHT
 	else:
-		choices = [True] * 80 + [False] * 20
+		choices = [1] * P_WORK_DAYTIME + [0] * (100 - P_WORK_DAYTIME)
 		
 	return random.choice(choices)
 
+def office_light_on(hour, forget_probability):
+	is_occupant_in = occupant_in(hour)
+        light_on_auto = is_dark(hour) and is_occupant_in
+        light_on_manual = light_on_auto
+
+
+	#check wither lights should be on
+	if not light_on_manual:
+		light_on_manual = random.choice([1] * forget_probability + [0] * (100 - forget_probability))
+	return {'auto': light_on_auto, 'manual': light_on_manual}
 
 if __name__ == "__main__":
 	GPIO.setmode(GPIO.BCM)
@@ -68,28 +82,39 @@ if __name__ == "__main__":
 	file_name = "report_%s.csv" % datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
 	file = open(file_name, "a")
 	if os.stat(file_name).st_size == 0:        
-		file.write("Day,Hour,Room,Dark,Cool,OccupantIn,LightOn,CorridorLightOn, AC \n")
+		file.write("Day,Hour,Room,Dark,Cool,OccupantIn,OfficeLightOnAuto,OfficeLightOnManuel,CorridorLightOnAuto,CorridorLightOnManual, AC \n")
 	day = 1
 
 	# Light the LED on and off in a random manner
 	for day in range(365):
 	    print "Day %i" % (day + 1)
 	    for hour in range(24):
-			atleast_one_occupant = False
+			atleast_one_occupant = 0
 			for room in rooms:
 			    is_occupant_in = occupant_in(hour)
-                            light_on = is_dark(hour) and is_occupant_in
+			    light_on = office_light_on(hour, P_FORGET_SWITCH_OFF_OFFICE_LIGHT)
+
+			    office_light_on_manual = light_on['manual']
+			    office_light_on_auto = light_on['auto']
+
                             ac_on = not is_cool(hour) and is_occupant_in
-			    atleast_one_occupant = atleast_one_occupant or is_occupant_in 
 
 			    #office lights
-			    if light_on:
+			    if office_light_on_auto:
 			    	GPIO.output(room['light'],GPIO.HIGH)
 			    else:
 			    	GPIO.output(room['light'],GPIO.LOW)
 
 		            #corridor light
-			    if atleast_one_occupant and is_dark(hour):
+			    atleast_one_occupant = atleast_one_occupant or is_occupant_in 
+			    corridor_light_on_auto = atleast_one_occupant and is_dark(hour)
+			    #forget factor
+			    corridor_light_on_manual = corridor_light_on_auto
+			    if not corridor_light_on_manual:
+			    	corridor_light_on_manual = random.choice([1] * P_FORGET_SWITCH_OFF_CORRIDOR_LIGHT + [0] * (100 -  P_FORGET_SWITCH_OFF_CORRIDOR_LIGHT))
+			    
+			    
+			    if corridor_light_on_auto:
 			    	GPIO.output(6,GPIO.HIGH)
 			    else:
 			    	GPIO.output(6,GPIO.LOW)
@@ -102,11 +127,11 @@ if __name__ == "__main__":
 			    	GPIO.output(room['ac'],GPIO.LOW)
 
 			    #write to csv file
-			    file.write("%i,%i,%s,%s,%s,%s,%s,%s,%s \n" % (day,hour,room['room_number'],str(is_dark(hour)), str(is_cool(hour)), str(occupant_in(hour)),str(light_on), str(atleast_one_occupant),str(ac_on)))
+			    file.write("%i,%i,%s,%s,%s,%s,%s,%s,%s,%s,%s \n" % (day,hour,room['room_number'],str(is_dark(hour)), str(is_cool(hour)), str(occupant_in(hour)),str(office_light_on_auto),str(office_light_on_manual), str(corridor_light_on_auto),str(corridor_light_on_manual),str(ac_on)))
 			    file.flush() 
 
 			    #display results
-			    print "Room Number %s Occupant is %s Light: %s and AC: %s" % (room['room_number'], "In" if is_occupant_in else "Out", "On" if light_on else "Off", "ON" if ac_on else "OFF")
+			    print "Room Number %s Occupant is %s Light: %s and AC: %s" % (room['room_number'], "In" if is_occupant_in else "Out", "On" if office_light_on_auto else "Off", "ON" if ac_on else "OFF")
 	    		print "Time: %i:00 It is %s and %s" % (hour, "dark" if is_dark(hour) else "bright", "cool" if is_cool(hour) else "hot") 
 			time.sleep(1)
 
